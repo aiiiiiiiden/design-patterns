1e9ed0736d399505dfe1d2e4268fbad70bdaad18
aiden <aiiiiiiiden@gmail.com>
2025-10-08 23:54:46 +0900

    Template Method Pattern, Strategy Pattern 코드 및 문서 정리

--- flutter/behavioral/strategy_pattern/lib/strategies/data_serializer_strategy.dart
+++ flutter/behavioral/strategy_pattern/lib/strategies/data_serializer_strategy.dart
@@ -2,7 +2,7 @@ import '../models/user.dart';
 
 /// Strategy interface for data serialization
 /// This defines the contract that all concrete serialization strategies must follow
-abstract class DataSerializerStrategy {
+abstract interface class DataSerializerStrategy {
   /// Serialize a list of users to a string in the specific format
   String serialize(List<User> users);
 
